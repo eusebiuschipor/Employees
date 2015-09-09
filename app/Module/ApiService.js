93ee2c038380333b85d381f2950854c4f79e5861
eusebiuschipor <eusebiu_schipor@yahoo.com>
2015-09-10 01:04:14 +0300

    Added ApiService.js

---
+++ app/Module/ApiService.js
@@ -0,0 +1,29 @@
+(function() {
+	app.service('ApiService', ['$http', '$window', '$location', function ($http, $window, $location) {
+        this.get = function(url, success, failure) {    
+            $http.get(url)          
+                .then(function (result) {   
+                    success(result);                                                       
+                }, function (error) {    
+                    if (error.status == '401') {       
+                        $location.path('/401');                                   
+      				else if (failure != null) {   
+                        failure(error);  
+                    }     
+                });      
+      	}
+
+      	this.post = function(url, data, success, failure) {    
+            $http.get(url, data)          
+                .then(function (result) {   
+                    success(result);                                                       
+                }, function (error) {    
+                    if (error.status == '401') {       
+                        $location.path('/401');                                   
+      				else if (failure != null) {   
+                        failure(error);  
+                    }     
+                });      
+      	}
+    }]);
+})();
